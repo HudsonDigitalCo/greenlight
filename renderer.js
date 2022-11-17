2ea74381a1e27af5b552e32e2bdc5b4060808e92
matthewhenry <matthewhenry@slack-corp.com>
2022-11-17 16:24:33 -0500

    running infinitely

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { mouse, left, right, up, down, straightTo, centerOf, Region} = require("@nut-tree/nut-js");
+const { mouse, left, right, up, down } = require("@nut-tree/nut-js");
 
 const square = async () => {
   await mouse.move(right(500));
@@ -9,7 +9,44 @@ const square = async () => {
   await mouse.move(up(500));
 };
 
-document.querySelector("#btnStart").addEventListener("click", async () => {
-  await square();
-  await mouse.move(straightTo(centerOf(new Region(100, 100, 200, 300))));
+const sleep = (sec) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, sec * 1000);
+  });
+};
+
+class Running {
+  constructor() {
+    this.active = false;
+  }
+
+  getActive = () => {
+    return this.active;
+  };
+
+  setActive = (status) => {
+    this.active = status;
+  };
+
+  move = async () => {
+    while (running.getActive()) {
+      await square();
+      await sleep(3);
+    }
+    console.log("Stopping move")
+  };
+}
+
+const running = new Running();
+
+document.querySelector("#state").addEventListener("click", async () => {
+  if (running.getActive()) {
+    document.querySelector("#state").value = "Start";
+    running.setActive(false);
+  } else {
+    document.querySelector("#state").value = "Stop";
+    await sleep(.5);
+    running.setActive(true);
+    running.move();
+  }
 });
